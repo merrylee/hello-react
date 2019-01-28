a7c6c94d8b9285c4450880a8af9bd28754838f04
seonmin <ismpink@daum.net>
2019-01-28 17:17:03 +0900

    classComponent

--- src/index.js
+++ src/index.js
@@ -4,8 +4,27 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const myElem = <p>이렇게써도되는거냐?</p>;
 
+const MyCom = () => <p>나도몰라..OTL</p>;
+
+class MyClassCom extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>클래스형 컴포넌트</p>
+        <MyCom />
+      </div>
+    );
+  }
+}
+
+// console.log(myCom);
+// console.log(myElem, myCom(), <myCom />);
+console.log(MyClassCom);
+console.log(<MyClassCom />);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
