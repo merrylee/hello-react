86b54d68174936ff47c63e6223de5bb0290c5049
seonmin <ismpink@daum.net>
2019-01-29 13:11:48 +0900

    timer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
+function tick() {
+  console.log("tick");
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+}
+
+const nTimer = setInterval(tick, 1000);
+
+const nTimer2 = setTimeout(function() {
+  clearInterval(nTimer);
+}, 5000);
+
+// setInterval(tick, 1000);
+
+/*
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const myElem = <p>이렇게써도되는거냐?</p>;
+const MyElem = <p>HTML태그를쓴 컴포넌트</p>;
+const MyCom = () => <p>HTML태그를쓴 컴포넌트</p>;
 
-const MyCom = () => <p>나도몰라..OTL</p>;
+console.log(MyElem, MyCom());
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 class MyClassCom extends React.Component {
   render() {
@@ -18,14 +46,13 @@ class MyClassCom extends React.Component {
     );
   }
 }
-
 // console.log(myCom);
 // console.log(myElem, myCom(), <myCom />);
 console.log(MyClassCom);
 console.log(<MyClassCom />);
 
-ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
