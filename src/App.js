b758f91f3e14dc511a26db71e53dfa0258e7c90d
seonmin <ismpink@daum.net>
2019-01-29 15:49:46 +0900

    Todos 컴포넌트로 변경 및 props 테스트

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, {Component} from "react";
 import logo from "./logo.svg";
 // import "./App.css";
-import LectureGoalList from "./components/LectureGoalList";
+import Todos from "./components/LectureGoalList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Todos
+          title={"강의목표"}
+          items={[
+            "React 개발에 필요한 환경을 구축한다.",
+            "새로운 자바스크립트 문법을 익힌다. ES2015(ES6), ES7, ES8",
+            "개발 편의를 위한 VSCode IDE를 익힌다.",
+            "기본적인 Git 사용법을 익힌다.",
+            "PR 코드 리뷰를 응용한 개발 프로세스를 익힌다.",
+            "React로 간단한 노트 앱을 만들어본다.",
+          ]}
+        />
         {/*        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -31,7 +42,7 @@ class App extends Component {
             <li>React로 간단한 노트 앱을 만들어본다.</li>
           </ul>
         </div>*/}
-        <LectureGoalList />
+        {/*<LectureGoalList />*/}
       </div>
     );
   }
--- src/components/LectureGoalList.jsx
+++ src/components/LectureGoalList.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 
-const LectureGoalList = () => (
-  <div className="LectureGoalList">
-    <h3>강의 목표</h3>
-    <dl>
-      <dd><input type="checkbox" />React 개발에 필요한 환경을 구축한다.</dd>
-      <dd><input type="checkbox" />새로운 자바스크립트 문법을 익힌다. ES2015(ES6), ES7, ES8</dd>
-      <dd><input type="checkbox" />개발 편의를 위한 VSCode IDE를 익힌다.</dd>
-      <dd><input type="checkbox" />기본적인 Git 사용법을 익힌다.</dd>
-      <dd><input type="checkbox" />PR 코드 리뷰를 응용한 개발 프로세스를 익힌다.</dd>
-      <dd><input type="checkbox" />React로 간단한 노트 앱을 만들어본다.</dd>
-    </dl>
-{/*    <ul>
-      <li><input type="checkbox" />React 개발에 필요한 환경을 구축한다.</li>
-      <li><input type="checkbox" />새로운 자바스크립트 문법을 익힌다. ES2015(ES6), ES7, ES8</li>
-      <li><input type="checkbox" />개발 편의를 위한 VSCode IDE를 익힌다.</li>
-      <li><input type="checkbox" />기본적인 Git 사용법을 익힌다.</li>
-      <li><input type="checkbox" />PR 코드 리뷰를 응용한 개발 프로세스를 익힌다.</li>
-      <li><input type="checkbox" />React로 간단한 노트 앱을 만들어본다.</li>
-    </ul>*/}
-  </div>
-);
+const Todos = (props) => {
 
-export default LectureGoalList;
+  console.log('>>>>>>>>>>>', props);
+
+  return (
+    <div className="Todos">
+      <h3>{props.title}</h3>
+      <dl>
+        <dd><input type="checkbox"/>{props.items[0]}</dd>
+        <dd><input type="checkbox"/>새로운 자바스크립트 문법을 익힌다. ES2015(ES6), ES7, ES8</dd>
+        <dd><input type="checkbox"/>개발 편의를 위한 VSCode IDE를 익힌다.</dd>
+        <dd><input type="checkbox"/>기본적인 Git 사용법을 익힌다.</dd>
+        <dd><input type="checkbox"/>PR 코드 리뷰를 응용한 개발 프로세스를 익힌다.</dd>
+        <dd><input type="checkbox"/>React로 간단한 노트 앱을 만들어본다.</dd>
+      </dl>
+    </div>
+  );
+};
+
+export default Todos;
