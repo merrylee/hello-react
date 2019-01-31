8c97456d2784873c7c5a37f60d3b1b4485c0d3c6
seonmin <ismpink@daum.net>
2019-01-31 15:52:27 +0900

    Counter 추가예제 구현

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,12 @@
-import React, {Component} from "react";
+import React, {PureComponent} from "react";
 import {Button, Input, Modal} from "antd";
 import "./Counter.less";
 
 const Search = Input.Search;
 
-class Counter extends Component {
-  // state = {
-  //   count: 0,
-  // };
-
+class Counter extends PureComponent {
   constructor(props) {
     super(props);
-
     this.count = 0;
     this.state = {
       open: false,
@@ -20,46 +15,25 @@ class Counter extends Component {
   }
 
   handleClick = e => {
-    // this.setState(prevState => {
-    //   return {count: prevState.count + 1};
-    // });
-    // this.setState(prevState => {
-    //   return {count: prevState.count + 1};
-    // });
-    // this.setState(prevState => {
-    //   return {count: prevState.count + 1};
-    // });
-    // this.setState(prevState => {
-    //   return {count: prevState.count + 1};
-    // });
+    const {list} = this.state;
+    // let num = Math.random() * 1000;
+    // num = Math.ceil(num);
 
-    this.count = this.count + 1;
+    // list.push(Math.ceil(num));
+    const num = list.filter(n => {
+      return n % 2 === 1;
+    });
 
-    if (this.count === 10) {
-      this.forceUpdate();
-    }
-
-    // this.setState({count: this.state.count + 1});
-    // this.setState({count: this.state.count + 1});
-    // this.setState({count: this.state.count + 1});
-    // this.setState({count: this.state.count + 1});
+    this.setState({list: num});
   };
 
   render() {
     return (
       <div className="Counter">
         <header className="App-header">
-          {/*{this.state.count}*/}
-          {this.count}
-
-          <Input.Search
-            className={"Search"}
-            placeholder="input search text"
-            onSearch={value => console.log(value)}
-            enterButton
-            style={{width: 320}}
-          />
-
+          {this.state.list.map((item, idx) => {
+            return <div key={`list-item${idx}`}>{item}</div>;
+          })}
           <Button onClick={this.handleClick}>카운터</Button>
         </header>
       </div>
